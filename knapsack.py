1347e4ff41e17dba2f892cfc8929c7f66cc93504
tamashiro0226 <e255748@ie.u-ryukyu.ac.jp>
2026-06-26 16:54:08 +0900

    ナップサック問題を総当たり法で解くコードを追加

---
+++ knapsack.py
@@ -0,0 +1,45 @@
+items = [
+    (1,4,6),
+    (2,8,12),
+    (3,3,4),
+    (4,5,3),
+    (5,9,7),
+    (6,2,1),
+    (7,3,3),
+    (8,1,2),
+    (9,5,7),
+    (10,2,3),
+    (11,4,4),
+    (12,2,2),
+    (13,7,10),
+    (14,10,13),
+    (15,3,5),
+    (16,13,16),
+    (17,11,14),
+    (18,8,9),
+    ]
+
+max_capacity = 45
+
+n = len(items)
+max_value = 0
+best_items = []
+
+for bit in range(1 << n):
+    total_capacity = 0
+    total_value = 0
+    selected = []
+
+    for i in range(n):
+        if bit & (1 << i):
+            item_number, capacity, value = items[i]
+            total_capacity += capacity
+            total_value += value
+            selected.append(item_number)
+
+            if total_capacity <= max_capacity and total_value > max_value:
+                max_value = total_value
+                best_items = selected
+
+print("選んだ品物:", best_items)
+print("最大値段:", max_value)
